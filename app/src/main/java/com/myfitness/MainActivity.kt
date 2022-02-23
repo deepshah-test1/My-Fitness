1c022053290d5579c1df1649db679ee2f837da93
deepshah-test <deepshah.test@gmail.com>
2022-02-23 18:46:20 +0530

    working code added, api integration,call dial-email send done

--- app/src/main/java/com/myfitness/MainActivity.kt
+++ app/src/main/java/com/myfitness/MainActivity.kt
@@ -1,11 +1,56 @@
 package com.myfitness
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.activity.viewModels
+import androidx.annotation.RequiresApi
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.gson.Gson
+import com.myfitness.adapters.UserRvAdapter
+import com.myfitness.repository.Resource
+import com.myfitness.viewmodel.MyFitnessViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+
+    private val viewModel : MyFitnessViewModel by viewModels()
+    private val userRvAdapter : UserRvAdapter by lazy { UserRvAdapter() }
+
+
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        rvUserList.adapter = userRvAdapter
+        rvUserList.layoutManager = LinearLayoutManager(this)
+
+        viewModel.userLiveData.observe(this, Observer {
+            when(it){
+                is Resource.Loading -> {
+                    rvUserList.visibility = View.GONE
+                    shimmerFrameLayout.visibility = View.VISIBLE
+                }
+                is Resource.Success -> {
+                    shimmerFrameLayout.visibility = View.GONE
+                    rvUserList.visibility = View.VISIBLE
+                    Log.e("API Call",Gson().toJson(it.data))
+                    it.data?.results?.let { userList -> userRvAdapter.setData(userList) }
+
+                }
+                is Resource.Error -> {
+                    shimmerFrameLayout.visibility = View.GONE
+                    Log.e("API Error",it.errorMessage.toString())
+                }
+            }
+        })
+
     }
 }
---
+++ app/src/main/java/com/myfitness/repository/Resource.kt
@@ -0,0 +1,11 @@
+package com.myfitness.repository
+
+sealed class Resource<T>(val data : T? = null,
+                        val errorMessage : String? = null){
+
+    class Loading<T> : Resource<T>()
+    class Success<T>(data : T? = null) : Resource<T>(data = data)
+    class Error<T>(errorMessage : String) : Resource<T>(errorMessage = errorMessage)
+
+}
+
